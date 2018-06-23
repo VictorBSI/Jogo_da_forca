ec3fbe42d48c9bae3c2e5180b1816b5fd5bb0d99
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-22 23:18:34 -0300

    Add files via upload

---
+++ Rank.java
@@ -0,0 +1,18 @@
+package jogo;
+
+public class Rank implements Resultados{
+	private int pontos;
+	private String nome;
+	
+
+	public Rank(int pontos, String nome) {
+		super();
+		this.pontos = pontos;
+		this.nome = nome;
+	}
+
+	public void mostrarResultados() {
+		
+	}
+	
+}
