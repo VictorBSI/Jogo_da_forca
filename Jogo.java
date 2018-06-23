32335f3804badb2aa56d230c78edee6a82c2fd81
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-22 23:15:25 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,4 +1,4 @@
-package forca;
+package jogo;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,6 +12,9 @@ public class Jogo implements Resultados{
 		Roleta row;
 		String[] corpo = {"Cabeça", "Tronco", "Braço esquerdo", "Braço direito", "Perna Esquerda", "Perna Direita"};
 		int acerto = 0, cont = 0;
+		Scanner teclado = new Scanner(System.in);
+		Boneco boneco = new Boneco();
+		Jogador jogador = new Jogador();
 		
 		public void iniciarJogo(Jogador player) {
 			this.player = new Jogador[2];
@@ -28,7 +31,8 @@ public class Jogo implements Resultados{
 	}
 	
 	public void Jogar(String animais) {
-			Scanner teclado = new Scanner(System.in);
+			
+			
 
 			char[] traco = new char[animais.length()];;
 			
@@ -38,18 +42,20 @@ public class Jogo implements Resultados{
 				}
 				while(achou) {
 					erro = true;
-					System.out.println("\nDigite uma letra: ");
-					char palavras = teclado.next().charAt(0);
+					/*System.out.println("\nDigite uma letra: ");
+					char palavras = teclado.next().charAt(0);*/
+					jogador.escolherLetra();
+					
 					
 					
 					for( int i = 0; i < animais.length(); i++) {
 						
-						if (palavras == traco[i]) {
+						if (jogador.getLetra() == traco[i]) {
 							System.out.print(traco[i]+" ");
 							continue;
 						}
-						if( palavras == animais.charAt(i)) {
-							traco[i] = palavras;
+						if( jogador.getLetra()  == animais.charAt(i)) {
+							traco[i] = jogador.getLetra() ;
 							acerto++;
 							erro = false;
 							
@@ -60,24 +66,77 @@ public class Jogo implements Resultados{
 						System.out.print(traco[i]+ " ");	
 					}
 					if (erro == true) {
-						System.out.println("\n\n"+corpo[cont]);
-						cont++;
+						jogador.Errar(erro);
+						++cont;
+						switch(cont) {
+						case 1:
+							boneco.setCabeca("cabeca");
+							boneco.setTronco("");
+							boneco.setB_drt("");
+							boneco.setB_esq("");
+							boneco.setP_drt("");
+							boneco.setP_esq("");
+							break;
+						case 2:
+							boneco.setTronco(" , tronco");
+							boneco.setB_drt("");
+							boneco.setB_esq("");
+							boneco.setP_drt("");
+							boneco.setP_esq("");
+							break;
+						case 3:
+							boneco.setB_drt(" , braço direito");
+							boneco.setB_esq("");
+							boneco.setP_drt("");
+							boneco.setP_esq("");
+							break;
+						case 4:
+							boneco.setB_esq(" , braco esquerda");
+							boneco.setP_drt("");
+							boneco.setP_esq("");
+							break;
+						case 5:
+							boneco.setP_drt(" , perna direita");	
+							boneco.setP_esq("");
+							break;
+						case 6:
+							boneco.setP_esq(" , perna esquerda");
+							break;
+						}
+						
+							System.out.println("\n\n"+boneco.toString());				
+						
+						
 
 					}
+					
 					if (cont == 6) {
 						System.out.println("\n\nVocê perdeu!");
 						achou = false;
+						
+						
+						
 					}
 
 					if(acerto == animais.length()) {
-						System.out.println("\n\nVocê ganhou!");
+						System.out.println("\n\nVocê acertou!");
 						achou = false;
+						//jogador.Acertar(achou);
+						
+						
 					}
 					
 				}
 				
+				System.out.println("\n\nVocê fez " + jogador.getPontos() + " pontos!\n\n");
 				
-					
 				
+				
+	}
+	
+	
+	
+	public int Pontos() {
+		return jogador.getPontos();
 	}
 }
