b422b5f3eb15da7e42c42932cafc030e8c241f3c
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-22 23:16:03 -0300

    Update and rename Main to Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-package forca;
+package jogo;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+	private static int i = 0;
 	
 	public static void main(String... args) {
 		Roleta roleta = new Roleta();
@@ -16,12 +17,15 @@ public class Main {
 			System.out.println(e.getMessage());
 			main();
 		}
-		
-		roleta.Roleta();
+
+		roleta.iniciarRoleta();
 		roleta.rodarRoleta();
-		//roleta.mostrarResultados();
+		
 		System.out.println("player 1 " + pl.getNome());
-		jogo.Jogar(roleta.getAnimal());
-		//System.out.println(roleta.getAnimais());
+		jogo.Jogar(roleta.getAnimais());
+	
+		Rank rank = new Rank(jogo.Pontos(), pl.getNome());
+		rank.mostrarResultados();
+
 	}
 }
