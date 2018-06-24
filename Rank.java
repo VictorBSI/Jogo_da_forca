513a9e9912b83952ba6740def6d314ef29d8124e
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-24 13:57:30 -0300

    Update Rank.java

--- Rank.java
+++ Rank.java
@@ -1,18 +1,30 @@
 package jogo;
 
-public class Rank implements Resultados{
-	private int pontos;
-	private String nome;
-	
+import java.util.*;
 
-	public Rank(int pontos, String nome) {
-		super();
-		this.pontos = pontos;
-		this.nome = nome;
+class Rank {
+	List<Jogador> rank = new ArrayList<>();
+
+	public void adicionar(Jogador j) {
+		if(rank.contains(j)) 
+			rank.add(rank.indexOf(j), j);
+		else
+			rank.add(j);	
 	}
 
-	public void mostrarResultados() {
-		
+	public List<Jogador> getLista() {
+		return rank;
 	}
-	
+
+	public void mostrarRank() {
+		Collections.sort(rank);
+		Collections.reverse(rank);
+		System.out.println("Posição | Nome | Vitórias | Derrotas |");
+		for (int i = 0; i < rank.size(); i++) {
+			System.out.printf("%8d|%6s|%10d|%10d|\n", i + 1, (rank.get(i).getNome()), rank.get(i).getVitorias(),
+					rank.get(i).getDerrotas());
+		}
+
+	}
+
 }
