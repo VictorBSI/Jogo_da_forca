2fe30e4639f7af14e933f3b5315240c27ad54c70
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-24 13:56:58 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -9,12 +9,26 @@ public class Jogo implements Resultados{
 		boolean achou = true;
 		boolean erro;
 		Jogador[] player;
-		Roleta row;
-		String[] corpo = {"Cabeça", "Tronco", "Braço esquerdo", "Braço direito", "Perna Esquerda", "Perna Direita"};
 		int acerto = 0, cont = 0;
 		Scanner teclado = new Scanner(System.in);
 		Boneco boneco = new Boneco();
 		Jogador jogador = new Jogador();
+		
+		public Jogo(Jogador player, Boneco boneco) {
+			this.jogador = player;
+			this.boneco = boneco;
+		}
+		
+		public int menu() {
+			System.out.println("1 - para continuar");
+			System.out.println("2 - para Jogar com outro player");
+			System.out.println("3 - para sair e ver o rank");
+			Scanner teclado = new Scanner(System.in);
+			return teclado.nextInt();
+		}
+		public void montarJogo(Roleta roleta) {
+		
+		}
 		
 		public void iniciarJogo(Jogador player) {
 			this.player = new Jogador[2];
@@ -30,24 +44,37 @@ public class Jogo implements Resultados{
 		
 	}
 	
-	public void Jogar(String tema) {
-			
-
-			char[] traco = new char[tema.length()];;
-			
-				for( int i = 0; i < tema.length(); i++) {
-					traco[i] = '_';
-					System.out.print(traco[i]+" ");		
-				}
+	public void Jogar(Roleta roleta) {
+		cont = 0;
+		acerto = 0;	
+		boolean achou = true;
+		boolean erro;
+		System.out.printf("player %s \n", jogador.getNome());
+		roleta.rodarRoletaTemas();
+		System.out.println("\n" + roleta.getTemanome() + "\n");		
+		String tema = roleta.rodarRoletaEscolhido(roleta.getTemas());
+		char[] traco = new char[tema.length()];
+		for (int i = 0; i < tema.length(); i++) {
+			if (tema.charAt(i) == ' ') {
+				traco[i] = ' ';
+				acerto++;
+			}else if(tema.charAt(i) == '-') {
+				traco[i] = '-';
+				acerto++;
+			}else {
+			traco[i] = '_'; 
+			}
+			System.out.print(traco[i] + " ");
+		}
 				while(achou) {
 					erro = true;
 					/*System.out.println("\nDigite uma letra: ");
 					char palavras = teclado.next().charAt(0);*/
 					jogador.escolherLetra();
-		
+	
 					for( int i = 0; i < tema.length(); i++) {
 						
-						if (jogador.getLetra() == traco[i]) {
+						if (jogador.getLetra() == traco[i]) {	//Se a letra digitada for igual a uma que já foi digitada anteriormente, sai do loop e retorna true, penalizando o jogador
 							System.out.print(traco[i]+" ");
 							continue;
 						}
@@ -56,8 +83,7 @@ public class Jogo implements Resultados{
 							acerto++;
 							erro = false;
 							
-						}
-	
+						}		
 						System.out.print(traco[i]+ " ");	
 					}
 					if (erro == true) {
@@ -103,18 +129,18 @@ public class Jogo implements Resultados{
 
 					}
 					
-					if (cont == 6) {
-						System.out.println("\nVocê perdeu!");
+					if (cont == 6) {		//Se completar as 6 partes do corpo, achou recebe false e o loop para
+						System.out.println("\n\nVocê perdeu!");
 						achou = false;
-						
+						jogador.setDerrotas();
 						System.out.println("\nA resposta era " + tema);
 						
 					}
 
-					if(acerto == tema.length()) {
-						System.out.println("\nVocê acertou!");
+					if(acerto == tema.length()) {  //Se o player acertar todas as letras da palavra sorteada, achou recebe false e o loop para
+						System.out.println("\n\nVocê acertou!");
 						achou = false;
-						//jogador.Acertar(achou);
+						jogador.setVitorias();
 						System.out.println("\nVocê fez " + jogador.getPontos() + " pontos!\n\n");
 						
 					}
