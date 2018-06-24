ef8217c09226778dbef29243411c8e7d683903a9
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-24 13:57:14 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,30 +4,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	private static int i = 0;
 	
-	public static void main(String... args) {
-		Roleta roleta = new Roleta();
-		Jogo jogo = new Jogo();
-		Jogador pl = new Jogador();
+	public static void main(String[] args) {
 		
 		
+		int sentinela = 0;
+		Rank rank = new Rank();
+		while (sentinela != 3) {
+			Roleta roleta = new Roleta();
+			Boneco boneco = new Boneco();
+			Jogador pl = new Jogador();
+			Jogo jogo = new Jogo(pl, boneco);
 		try {
 			pl.cadastroNome();
 		}catch(IllegalArgumentException e) {
 			System.out.println(e.getMessage());
-			main();
+			//main();
 		}
 
 		//roleta.iniciarRoleta();
 		
-		
-		System.out.println("player 1 " + pl.getNome());
-		roleta.rodarRoletaTemas();
-		System.out.println("\n" + roleta.getTemanome() + "\n");
-		jogo.Jogar(roleta.rodarRoletaEscolhido(roleta.getTemas()));
-		Rank rank = new Rank(jogo.Pontos(), pl.getNome());
-		rank.mostrarResultados();
+		do {
+			jogo.Jogar(roleta);
+			sentinela = jogo.menu();
+		} while (sentinela == 1);
+			rank.adicionar(pl);
+		}
+		rank.mostrarRank();
 
 	}
 }
