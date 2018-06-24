ba106063d5fef82839f143e8751aefb8febff169
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-23 23:11:11 -0300

    Update Roleta.java

--- Roleta.java
+++ Roleta.java
@@ -4,34 +4,73 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 public class Roleta extends Jogo implements Resultados{
-	private int tamanho;
-	private String[] animais;
+	private int tamanho1;
+	private int tamanho2;
+	private String[] temas, escolhido, nome;
+	private String temanome;
 	//private int animal_atual;
 	
-	public void iniciarRoleta() {
+	
+	/*public void iniciarRoleta() {
 		String[] animais = {"girafa","cachorro","gato","pato","morcego","lobo"};
 		this.animais = animais;
 		
-	}
+	}*/
 	
-	public String rodarRoleta(){
+	public String[] rodarRoletaTemas(){
+		Temas tema = new Temas();
 		SecureRandom random = new SecureRandom();
-		this.tamanho = random.nextInt(this.animais.length);
-		return this.animais[tamanho];
+		this.tamanho1 = random.nextInt(tema.getTema().length);
+		switch (tamanho1){
+			case 0:
+				this.temanome = "Animais";
+				return nome = tema.getAnimais();
+			case 1: 
+				this.temanome = "Objetos";
+				return nome = tema.getObjetos();
+			case 2: 
+				this.temanome = "Filmes";
+				return nome = tema.getFilmes();
+			case 3: 
+				this.temanome = "Profissões";
+				return nome = tema.getProfissoes();
+			case 4: 
+				this.temanome = "Frutas";
+				return nome = tema.getFrutas();
+			case 5: 
+				this.temanome = "Cidades";
+				return nome = tema.getCidades();
+			default: 
+				this.temanome = "Times";
+				return nome = tema.getTimes();
+		}
+		//return temas[tamanho1];
 	}
 	
 	
 	
-	public String getAnimais(){
-		return this.animais[tamanho];
+	public String[] getTemas(){
+		return this.nome;
+	}
+	
+	public String getTemanome() {
+		return this.temanome;
 	}
 	
 	@Override
 	public void mostrarResultados(){
 		//SecureRandom random = new SecureRandom();
 		//this.tamanho = animais.length;
-		System.out.println(tamanho);
+		System.out.println(tamanho1);
 	}
+	
+	public String rodarRoletaEscolhido(String[] escolhido) {
+		SecureRandom random = new SecureRandom();
+		this.tamanho2 = random.nextInt(escolhido.length);
+		return escolhido[tamanho2];
+	}
+	
+	
 	
 	/*public int tamanho(){
 		return tamanho;
