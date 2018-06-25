d24bd0a4d1ebb11a395868ce0cd5066e454a5883
VictorBSI <38796125+VictorBSI@users.noreply.github.com>
2018-06-24 23:50:11 -0300

    Update Temas.java

--- Temas.java
+++ Temas.java
@@ -1,7 +1,7 @@
 package jogo;
 
-public class Temas {
-	private String[][] temas = {{"cachorro", "gato", "jacaré", "gaivota", "pato", "morcego", "elefante", "girafa", "macaco", "baleia", "rato", "lobo", "tigre", "panda", "urso", "tubarão", "lagarto", "sapo", "cobra", "vaca", "boi", "touro", "escorpião", "aranha", "barata", "gavião", "zebra", "ornitorrinco", "beija-flor"}, {"cama", "mesa", "ventilador", "geladeira", "fogão", "chave", "televisão", "rádio", "computador", "armário", "telefone", "celular", "cadeira", "pia", "escada", "carro", "bicicleta", "termômetro", "mouse"}, {"titanic", "matrix", "avatar", "kill bill", "as crônicas de nárnia", "harry potter", "o senhor dos aneis", "as panteras", "karatê kid", "motoqueiro fantasma", "homem-aranha"}, {"lenhador", "professor", "médico", "administrador", "carpinteiro", "advogado", "juíz", "promotor", "engenheiro", "enfermeiro", "pintor", "cantor", "ator", "atriz", "modelo", "diretor", "motorista", "telefonista", "ginasta", "gerente", "nadador", "vendedor"}, {"maçã", "abacaxi", "melão", "melancia", "pêra", "mamão", "abacate", "laranja", "tangerina", "banana", "açaí", "pupunha", "ameixa"}, {"belém", "maceió", "manaus", "recife", "londres", "paris", "lisboa", "brasília", "fortaleza", "cuiabá", "florianópolis", "madrid", "palmas", "curitiba", "são paulo", "rio de janeiro", "belo horizonte", "porto alegre", "salvador", "são luís", "macapá"}, {"paysandu", "remo", "tuna", "corinthians", "palmeiras", "juventude", "bahia", "fortaleza", "flamengo", "barcelona", "grêmio", "internacional", "cruzeiro", "são paulo", "real madrid"}};
+public class Temas implements Resultados{
+	private String[][] temas = {{"cachorro", "gato", "jacaré", "gaivota", "pato", "morcego", "elefante", "girafa", "macaco", "baleia", "rato", "lobo", "tigre", "panda", "urso", "tubarão", "lagarto", "sapo", "cobra", "vaca", "boi", "touro", "escorpião", "aranha", "barata", "gavião", "zebra", "ornitorrinco", "beija-flor"}, {"cama", "mesa", "ventilador", "geladeira", "fogão", "chave", "televisão", "rádio", "computador", "armário", "telefone", "celular", "cadeira", "pia", "escada", "carro", "bicicleta", "termômetro", "mouse"}, {"titanic", "matrix", "avatar", "kill bill", "as crônicas de nárnia", "harry potter", "o senhor dos anéis", "as panteras", "karatê kid", "motoqueiro fantasma", "homem-aranha"}, {"lenhador", "professor", "médico", "administrador", "carpinteiro", "advogado", "juíz", "promotor", "engenheiro", "enfermeiro", "pintor", "cantor", "ator", "atriz", "modelo", "diretor", "motorista", "telefonista", "ginasta", "gerente", "nadador", "vendedor"}, {"maçã", "abacaxi", "melão", "melancia", "pêra", "mamão", "abacate", "laranja", "tangerina", "banana", "açaí", "pupunha", "ameixa"}, {"belém", "maceió", "manaus", "recife", "londres", "paris", "lisboa", "brasília", "fortaleza", "cuiabá", "florianópolis", "madrid", "palmas", "curitiba", "são paulo", "rio de janeiro", "belo horizonte", "porto alegre", "salvador", "são luís", "macapá"}, {"paysandu", "remo", "tuna", "corinthians", "palmeiras", "juventude", "bahia", "fortaleza", "flamengo", "barcelona", "grêmio", "internacional", "cruzeiro", "são paulo", "real madrid"}};
 	private String[] animais, objetos, filmes, profissoes, frutas, cidades, times;
 	
 	//objetos, frutas, filmes;
@@ -38,7 +38,9 @@ public class Temas {
 	public String[][] getTema() {
 		return this.temas;
 	}
-	
-
+	@Override
+	public void mostrarResultados() {
+		System.out.println("\nTemas: Animais, Objetos, Filmes, Profissões, Frutas, Cidades, Times\n");
+	}
 
 }
